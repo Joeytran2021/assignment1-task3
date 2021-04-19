a660478a1f181ce5f4e748c098cbddc39ad1c9fc
Thi My Le Tran <79957926+Joeytran2021@users.noreply.github.com>
2021-04-20 01:52:34 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 # save as query.sql
 USE compbiol;
 SELECT a.gene, a.function_1, e.expr_value, a.metabolism
-FROM annotation AS a INNER JOIN expression AS e ON a.gene=e.gene;
+FROM annotation AS a JOIN expression AS e ON a.gene=e.gene;
 
     
