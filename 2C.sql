a3e1c33bded4bb026adf9ee7f2a76fa2365e4d0d
Thi My Le Tran <79957926+Joeytran2021@users.noreply.github.com>
2021-04-19 23:29:30 +0700

    Update 2C.sql

--- 2C.sql
+++ 2C.sql
@@ -1,3 +1,6 @@
 SELECT m.LastName, m.FirstName, m.MemberID
-    -> FROM Member m INNER JOIN Entry e ON m.MemberID = e.MemberID
-    -> WHERE NOT EXISTS (SELECT * FROM Entry e WHERE m.MemberID = e.MemberID AND e.Year = 2013);
+    -> FROM Member m
+    -> WHERE NOT EXISTS
+    -> (SELECT * FROM Entry e
+    -> WHERE e.MemberID = m.MemberID
+    -> AND e.Year <> 2013);
